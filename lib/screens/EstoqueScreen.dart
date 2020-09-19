e6e6bfea6b26d1d71f94d6d1fab4601fe06566bd
leoruas <leo.coelho.ruas@gmail.com>
2020-09-19 19:33:30 -0300

    Finished app basic navigation on main screens

---
+++ lib/screens/EstoqueScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EstoqueScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Estoque"),
+    );
+  }
+}
