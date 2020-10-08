87403b5d8b7e7747261ab3aa285d3d4a5e05d30f
rehpedroso <rebeca_pedroso6@hotmail.com>
2020-10-08 16:08:11 -0300

    Estoque Screen

--- lib/screens/EstoqueScreen.dart
+++ lib/screens/EstoqueScreen.dart
@@ -1,17 +1,30 @@
 import 'package:concessionaria_paiv/utils/Magic.dart';
+import 'package:concessionaria_paiv/widgets/HorizontalCardWidget.dart';
 import 'package:flutter/material.dart';
 
 class EstoqueScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Estoque"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.pushNamed(context, newCarRouteName),
-        child: Icon(Icons.add),
-        backgroundColor: Colors.red.shade900,
+    List<Widget> stockCar = <Widget>[
+      HorizontalCard(),
+      HorizontalCard(),
+      HorizontalCard(),
+      HorizontalCard(),
+      HorizontalCard(),
+      HorizontalCard(),
+    ];
+    return SingleChildScrollView(
+      child: Scaffold(
+        body: Column(
+          Text("Estoque"),
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: stockCar,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => Navigator.pushNamed(context, newCarRouteName),
+          child: Icon(Icons.add),
+          backgroundColor: Colors.red.shade900,
+        ),
       ),
     );
   }
--- lib/utils/Router.dart
+++ lib/utils/Router.dart
@@ -3,6 +3,7 @@ import 'package:concessionaria_paiv/utils/Magic.dart';
 import 'package:flutter/material.dart';
 
 class Router {
+  // ignore: missing_return
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case newCarRouteName:
